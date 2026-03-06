288f418c4bf6663b81ff947762fe2c77085cb016
garciab1 <esmeralda.garcia1@catolica.edu.sv>
2026-03-05 18:54:21 -0600

    Creacion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { ProductsComponent } from './pages/products/products.component'; //Importanto las rutas de los componentes 
 
-export const routes: Routes = [];
+//Definir las rutas
+export const routes: Routes = [
+    {
+        path: '',
+        component: InicioComponent
+    },
+
+    {
+        path: 'Categoria',
+        component: CategoriasComponent
+    },
+
+    {
+        path: 'Producto',
+        component: ProductsComponent
+    }
+
+];
